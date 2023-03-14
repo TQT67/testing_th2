e4a04de70ff1531ed7097f60fbc054d35dcc9bf7
TQT67 <trangtrinh000000@gmail.com>
2023-03-14 19:37:35 +0700

    th2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
 
-function App() {
+const BackgroundColor = () => {
+  const [number, setNumber] = useState(0);
+
+  const color = useMemo(() => {
+    if (number === 0) {
+      return "white";
+    }
+    if (number % 2 === 0) {
+      return "red";
+    }
+    if (number % 2 !== 0) {
+      return "blue";
+    }
+    return "white";
+  }, [number]);
+
+  const changeNumer = () => {
+    setNumber((curr) => {
+      return curr + 1;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={changeNumer} data-testid="button">
+        Click here
+      </button>
+      <div
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: color,
+        }}
+        data-testid="background"
+      ></div>
     </div>
   );
-}
+};
 
-export default App;
+export default BackgroundColor;
